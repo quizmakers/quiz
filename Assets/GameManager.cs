80d732f7ae67b3402c37fbaed849b08e9a639e9c
BialkoKlau <64467100+BialkoKlau@users.noreply.github.com>
2020-05-05 17:01:37 +0200

    Second Version

---
+++ Assets/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class GameManager : MonoBehaviour
+{
+    public Kerdes[] kerdes;
+    private static List<Kerdes> megvalaszolatlankerdes;
+
+    private Kerdes jelenlegiKerdes;
+    void Start()
+    {
+        if (megvalaszolatlankerdes == null || megvalaszolatlankerdes.Count == 0)
+        {
+            megvalaszolatlankerdes = kerdes.ToList<Kerdes>();
+        }
+
+        GetRandomKerdes();
+        Debug.Log(jelenlegiKerdes.teny + "a" + jelenlegiKerdes.igaz);
+    }
+
+    void GetRandomKerdes()
+    {
+        int randomKerdesIndex = Random.Range(0, megvalaszolatlankerdes.Count);
+        jelenlegiKerdes = megvalaszolatlankerdes[randomKerdesIndex];
+        megvalaszolatlankerdes.RemoveAt(randomKerdesIndex);
+    }
+}
---
+++ Assets/Kerdes.cs
@@ -0,0 +1,7 @@
+﻿[System.Serializable]
+public class Kerdes
+{
+    public string teny;
+    public bool igaz;
+    
+}
